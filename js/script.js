f2300efd2b6c31c2b5ff605fee341254101707aa
Lucy D <126295672+lucinda-d@users.noreply.github.com>
2024-11-08 14:23:09 -0600

    Fetching repo data

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 //create global variables
 const overview = document.querySelector(".overview");
 const username = "lucinda-d";
+const repoList = document.querySelector(".repo-list");
 
 //fetch API JSON data
 const grabProfileInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
   const data = await userInfo.json();
-  console.log(data);
+  // console.log(data);
   displayUser(data);
 };
+
 grabProfileInfo();
 
 //fetch and display user information
@@ -25,4 +27,25 @@ const displayUser = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
   overview.append(div);
+  repoFetch();
+};
+
+//fetch your repos
+const repoFetch = async function () {
+  const gitRepo = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+  );
+  const repoData = await gitRepo.json();
+  // console.log(repoData);
+  displayRepoInfo(repoData);
+};
+
+//display info about your repos
+const displayRepoInfo = function (repos) {
+  for (const repo of repos) {
+    const repoListItem = document.createElement("li");
+    repoListItem.classList.add("repo");
+    repoListItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoListItem);
+  }
 };
